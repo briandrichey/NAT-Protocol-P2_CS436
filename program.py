7a01e391a6418f7a5f1b293fc38a079265bef5c0
Brian D. Richey <62313053+briandrichey@users.noreply.github.com>
2021-11-29 14:12:35 -0800

    Update program.py

--- program.py
+++ program.py
@@ -8,17 +8,19 @@ def process_pkt_private(pkt: Packet):
         print("received private pkt", pkt.sniffed_on, pkt.summary())
         if ICMP in pkt:
             # Create a new IP packet with specified src and dst
-            newPort = (rand()%3000) - 1000
             
-            srcIp = get_if_addr(conf.iface)  # default interface
+            
+            srcIp = get_if_addr(conf.iface)  # gets source ip '0.0.0.0'
             srcIp = get_if_addr("eth0")
             
-            routIP = conf.route.route("0.0.0.0")[2]
+            routIP = conf.route.route("0.0.0.0")[2] #gets router ip '0.0.0.0'
             
-            s=socket.socket()
+            newPort = (rand()%3000) - 1000
+            s=socket.socket() #makes connection to new / hidden port
             s.connect((ip,newPort))
             
             new_pkt = IP(src = routIp, dst="172.16.20.100") / pkt[ICMP]
+            
             # Send the new packet over the public interface
             send(new_pkt, iface=PUBLIC_IFACE, verbose=False)
             
