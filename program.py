9dac5920eaf35ed8fff583a5af9f97b285b14e8b
Brian D. Richey <62313053+briandrichey@users.noreply.github.com>
2021-11-29 14:26:05 -0800

    Update program.py

--- program.py
+++ program.py
@@ -1,7 +1,28 @@
-
+from scapy.sendrecv import send, sniff
+def private_listener():
+    print("sniffing packets on the private interface")
+    sniff(prn=process_pkt_private, iface=PRIVATE_IFACE, filter="icmp or tcp")
+    
+    
+    
+    
+    
+import threading
+def main():
+    thread1 = threading.Thread(target=private_listener)
+    thread2 = threading.Thread(target=public_listener)
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
+    
+    
+    
 
 from scapy.packet import Packet
 from scapy.layers.inet import TCP, IP, Ether, ICMP
+from random import seed
+from random import randint
 
 def process_pkt_private(pkt: Packet):
     if pkt.sniffed_on == PRIVATE_IFACE:
@@ -15,9 +36,11 @@ def process_pkt_private(pkt: Packet):
             
             routIP = conf.route.route("0.0.0.0")[2] #gets router ip///ie. '0.0.0.0'
             
-            newPort = (rand()%3000) - 1000
+            seed(1)
+            newPort = randint(2000, 2999) #chooses port in 2000 range
             s=socket.socket() #makes connection to new / hidden port
-            s.connect((srcIp,newPort))
+            s.connect((routIp,newPort))
+            ss=StreamSocket(s,Raw)
             
             new_pkt = IP(src = routIp, dst="172.16.20.100") / pkt[ICMP]
             
@@ -26,16 +49,6 @@ def process_pkt_private(pkt: Packet):
             
 #ping[172.16.20.100] tests client 1 and 2
 
-import threading
-def main():
-    thread1 = threading.Thread(target=private_listener)
-    thread2 = threading.Thread(target=public_listener)
-    thread1.start()
-    thread2.start()
-    thread1.join()
-    thread2.join()
 
-from scapy.sendrecv import send, sniff
-def private_listener():
-    print("sniffing packets on the private interface")
-    sniff(prn=process_pkt_private, iface=PRIVATE_IFACE, filter="icmp or tcp")
+
+
